f585b2b3bd70e4fd61e5e87e7e89b0daed3ff5d7
zeph254 <ularezephaniah@gmail.com>
2025-03-06 01:04:27 +0300

    Frontend-deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
 })
